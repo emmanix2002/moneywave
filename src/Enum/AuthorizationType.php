d830575fed189ac1948b1158c9444836bfe2b7b3
Okeke Emmanuel <emmanix2002@gmail.com>
2017-02-23 14:30:46 +0100

    added the ValidateTransfer service

---
+++ src/Enum/AuthorizationType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Emmanix2002\Moneywave\Enum;
+
+class AuthorizationType
+{
+    const ACCOUNT_CREDIT = 'ACCOUNT_CREDIT';
+    const OTP = 'OTP';
+    const PAY_VALIDATE = 'PAY_VALIDATE';
+}
